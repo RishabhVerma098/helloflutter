9991e01acdba7d029b448e960df3656897a0ad3c
rishabh verma <vermarishabh0987@gmail.com>
2019-04-13 12:46:38 +0530

    padding margin done

---
+++ lib/helper/helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Helper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.cyanAccent,
+        child: Text("Hello Bruh.."),
+        margin: EdgeInsets.only(top: 20, left: 50),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
-import './asserts/helperfile.dart';
-import 'dart:math';
+import './helper/helper.dart';
 
 void main() {
-  runApp(MyflutterApp());
-}
-
-class MyflutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Hello World",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                "Your Number is ${genrateLuckyNumber()}",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(color: Colors.black),
-              ),
-              backgroundColor: Colors.white12,
-            ),
-            body: Hellper()));
-  }
-}
-
-int genrateLuckyNumber() {
-  var random = Random();
-  int luckyNumber = random.nextInt(10);
-  return luckyNumber;
+  runApp(MaterialApp(
+    title: "Exploring Widgets",
+    debugShowCheckedModeBanner: false,
+    home: Helper(),
+  ));
 }
